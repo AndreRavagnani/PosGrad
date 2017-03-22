ad82f3a8288ed9a11f45e6030d8c8ac5ea871b65
unknown <posgrad@FA7-L30M24.7setlabs.local>
2017-03-22 20:30:45 -0300

    Teste

---
+++ PosGrad/src/Pincel.java
@@ -0,0 +1,23 @@
+
+public class Pincel {
+	String cor;
+	String forma;
+	int tamanho;
+	int capacidade;
+
+	public Pincel(){
+		this("Rosa", "Cilindrico", 0,0);
+	}
+
+	public Pincel(String cor, String forma, int tamanho, int capacidade){
+		this.cor = cor;
+		this.forma = forma;
+		this.tamanho = tamanho;
+		this.capacidade = capacidade;
+	}
+	
+	public void Riscar(String mensagem){
+		System.out.println("Riscando de cor " + this.cor + " a mensagem " + mensagem);
+	}
+
+}
