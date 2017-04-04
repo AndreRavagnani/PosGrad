8b834159a17ebc5b0ede1edb6c1268f8233680a7
Andre <andreskiter@gmail.com>
2017-04-03 22:19:04 -0300

    Final da aula

--- PosGrad/src/Circulo.java
+++ PosGrad/src/Circulo.java
@@ -1,10 +1,14 @@
 
-public class Circulo extends DesenhaFormas{
+public class Circulo extends DesenhaFormas {
 
+	/*
+	 * 4ª Questão a) Não, pois a implementação do diagrama requer refatoração
+	 * das classes.
+	 */
 	@Override
 	void desenhar() {
 		System.out.println("Circulo");
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 	}
 
 }
